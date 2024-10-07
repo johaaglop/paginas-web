5bddede0430bbf515b9cd13d341c89518eee3a2f
Johana AgLop <johana.aguilar04@gmail.com>
2024-10-07 16:06:47 -0600

    menu hamburguesa listo

---
+++ pagina web agencia de viajes/js/menu.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-navegacion');
+
+console.log(menu)
+console.log(hamburger)
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle("spread")
+})
